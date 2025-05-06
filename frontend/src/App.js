7bad21df844040afeee3254e9f9029782ebc8acf
Sanjay Kamath <sanjaykamath6969@gmail.com>
2025-05-06 07:06:10 +0530

    merge complete
    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,11 @@ import Signup from "./pages/Signup";
 import ComplaintForm from "./pages/ComplaintForm";
 import MyComplaints from "./pages/MyComplaints";
 import Home from "./pages/Home";
-<<<<<<< Updated upstream
-=======
 import OfficialLogin from "./pages/OfficialLogin";
 import AdminLogin from "./pages/AdminLogin";
 import OfficialDashboard from "./pages/OfficialDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 
->>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -28,14 +25,11 @@ const App = () => {
             <Route path="/signup" element={<Signup />} />
             <Route path="/complaints" element={<ComplaintForm />} />
             <Route path="/my-complaints" element={<MyComplaints />} />
-<<<<<<< Updated upstream
-=======
             <Route path="/official-login" element={<OfficialLogin />} />
             <Route path="/official-dashboard" element={<OfficialDashboard />} />
             <Route path="/official/dashboard" element={<OfficialDashboard />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/admin-login" element={<AdminLogin />} />
->>>>>>> Stashed changes
           </Routes>
         </main>
         <Footer />
